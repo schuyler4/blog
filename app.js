be9109a40de13af8c732321dd8c12bb9f204b19b
Marek Newton <marek.s.newton@gmail.com>
2016-08-13 14:44:16 -0600

    worked on stuff

--- app.js
+++ app.js
@@ -13,97 +13,100 @@ app.use(bodyParser.json())
 const secret = require('./secret.js')
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/blogdata')
+let collection
+mongoose.connect('mongodb://localhost/blogdata', function(err) {
+	if(err)console.err(err)
+})
+mongoose.Promise = global.Promise;
 
 mongoose.connection.on('connected', function () {
   console.log('Mongoose default connection open')
 })
 
 mongoose.connection.on('error',function (err) {
-  console.log('Mongoose default connection error: ' + err);
-});
+  console.log('Mongoose default connection error: ' + err)
+})
 
 let db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', function() {
 	console.log("the connection is open")
 })
 
 const Schema = mongoose.Schema
+
+var Comments = new Schema({
+	name: String,
+	body: String,
+	date: Date
+});
+
 const blogPostSchema = mongoose.Schema({
 		date: { type: Date, default: Date.now },
     title: String,
+		img: { data: Buffer, contentType: String },
 		content: String,
+		comments: [Comments]
 })
-
 let blogPost = mongoose.model('blogPost', blogPostSchema)
 
 app.get('/', (req, res)=> {
 	res.render('home')
 })
 
-app.get('/addPost',(req, res)=> {
+app.get('/admin',(req, res)=> {
 	res.render('addBlogPost')
 })
 
-app.post('/addPost', (req, res)=> {
-	let testPost = new blogPost({
-		title:"blogPostTitle",
-		content:"test content"
+app.post('/admin', (req, res)=> {
+	let Post = new blogPost({
+		title:req.body.title,
+		content:req.body.blogContent
 	})
 
-	testPost.save(function(err) {
+	Post.save(function(err) {
 		if(err) {
 			console.log(err)
 		}
 		else {
-			console.log(testPost)
+			console.log(Post)
 		}
+	})
+	res.redirect('/')
+})
+
+let id
+
+app.get('/blogPost/:id', function(req, res){
+
+	id = req.params.id
+
+	blogPost.findById(id, function(err, post) {
+		res.render('blogPost',{content:post.content, title:post.title,
+			date:post.date,comments:post.comments, commentName:post.comments.name,
+			commentBody:post.comments.body})
 	})
 })
 
-app.post('/', (req, res)=> {
-	console.log("panda")
-})
+app.post('/blogPost', function(req, res) {
+	console.log(id)
+	blogPost.findById(id, function(err, post) {
+		post.comments.push({ name: req.body.name, body: req.body.body });
 
-app.use('/admin', admin)
-
-admin.get('/', (req,res)=> {
-	console.log(admin.mountpath)
-	res.render('addBlogPost')
-})
-
-admin.post('/addBlogPost', (req, res)=> {
-	let post = new blogPost({
-		title: "testPost",
-		content: "this is test post content"
-})
-
-	post.save((err, post)=> {
-		if(err) {
-			console.error(err);
-		}
-		else {
-			console.dir(post)
-		}
-	})
-})
-
-let allBlogPosts = db.blogposts.find().toArray()
-
-for(i in allBlogPosts) {
-	console.log(blogPosts[i])
-	app.get('/' + 'blogPost' + i, (req, res)=> {
-		for(i in blogPosts) {
-			res.send(blogPosts[i])
-		}
-	})
-}
+		post.save(function (err) {
+			if (!err) console.log('Success!');
+		})
+		res.redirect('/blogPost/' + id);
+	});
+});
 
 app.get('/blogPostList', (req, res)=> {
-	res.render('allBlogPosts.hbs')
+	blogPost.find(function(err, data) {
+		console.log(data)
+		res.render('allBlogPosts',{data:data, title:data.title, id: data.id})
+	})
 })
 
 app.listen(3000, function () {
   console.log('listening on port 3000!');
-});
+})
