f14cda6b824df0c14f37d6176d252d430eef88d8
Marek Newton <marek.s.newton@gmail.com>
2016-08-15 21:01:47 -0600

    worked on stuff

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const admin = express()
+const fs = require('fs')
+app.disable('x-powered-by');
 
 const exphbs = require('express-handlebars');
 app.engine('.hbs', exphbs({defaultLayout: 'single', extname: '.hbs'}));
@@ -9,6 +11,7 @@ app.set('view engine', '.hbs');
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
+
 
 const secret = require('./secret.js')
 
@@ -33,37 +36,64 @@ db.once('open', function() {
 	console.log("the connection is open")
 })
 
-const Schema = mongoose.Schema
-
+const Schema = mongoose.Schema;
 var Comments = new Schema({
 	name: String,
 	body: String,
 	date: Date
 });
 
+const Grid = require("gridfs-stream");
+Grid.mongo = mongoose.mongo;
+let gfs;
+db.once("open", function() {
+	gfs = Grid(db.db)
+});
 const blogPostSchema = mongoose.Schema({
-		date: { type: Date, default: Date.now },
-    title: String,
-		img: { data: Buffer, contentType: String },
-		content: String,
-		comments: [Comments]
+	date: { type: Date, default: Date.now },
+  title: String,
+	upload: { data: Buffer, contentType: String },
+	content: String,
+	comments: [Comments]
 })
 let blogPost = mongoose.model('blogPost', blogPostSchema)
 
-app.get('/', (req, res)=> {
-	res.render('home')
+const homePageSchema = mongoose.Schema({
+	title: String,
+	content: String
 })
+
+let homePageInfo = mongoose.model('homePageInfo', homePageSchema)
+
+const imageSchema = mongoose.Schema({
+	img:{ data: Buffer, contentType: String }
+})
+const Image = mongoose.model('Image', imageSchema)
+
+const multer = require('multer')
+
+var upload = multer({dest: "./uploads"});
+
+
+app.get('/', (req, res)=> {
+	 homePageInfo.findOne({'title':'Aboutme'}, function(err, data) {
+		console.log(data)
+		res.render('home',{data:data.content})
+	});
+});
 
 app.get('/admin',(req, res)=> {
-	res.render('addBlogPost')
+	homePageInfo.findOne({'title':'Aboutme'}, function(err, data) {
+		res.render('addBlogPost',{data:data.content})
+	})
 })
 
-app.post('/admin', (req, res)=> {
+app.post('/admin', function(req, res) {
+	console.log("posting text")
 	let Post = new blogPost({
 		title:req.body.title,
-		content:req.body.blogContent
-	})
-
+		content:req.body.blogContent//,
+	});
 	Post.save(function(err) {
 		if(err) {
 			console.log(err)
@@ -71,14 +101,75 @@ app.post('/admin', (req, res)=> {
 		else {
 			console.log(Post)
 		}
-	})
+	});
 	res.redirect('/')
+});
+let homePageAdded = false
+
+if(!homePageAdded) {
+	app.post('/addHomePage', function(req, res) {
+		var homePage = new homePageInfo({
+			title: "Aboutme",
+			content: req.body.aboutMe
+		})
+		homePage.save(function (err) {
+			if (!err) console.log('Success!');
+		})
+		res.redirect('/')
+		homePageAdded = true
+	});
+}
+else {
+	app.put('url', function(req, res) {
+
+        // use our bear model to find the bear we want
+        Bear.findById(req.params.bear_id, function(err, bear) {
+
+            if (err)
+                res.send(err);
+
+            bear.name = req.body.name;  // update the bears info
+
+            // save the bear
+            bear.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'Bear updated!' });
+            });
+
+        });
+    });
+}
+
+/*app.post('/editBlogPost', function(req, res) {
+	let query = {"_id":id}
+	let updateTitle = {$set:{title: req.body.title, content: req.body.content}
+	blogPost.findOneAndUpdate(query, update, {new: true}, function(err, update) {
+		if(err) {
+			console.log(err)
+		}
+		else {
+			res.redirect("/blogPost/"id)
+		}
+	})
+})*/
+
+
+let id;
+
+app.get('/editBlogPost/:id', function(req, res) {
+	id = req.params.id
+	blogPost.findById(id, function(err, data) {
+		res.render("editBlogPost",{content:data.content, title:data.title})
+	})
 })
 
-let id
+app.post('/deleteBlogPost', function(req, res) {
+
+})
 
 app.get('/blogPost/:id', function(req, res){
-
 	id = req.params.id
 
 	blogPost.findById(id, function(err, post) {
@@ -89,7 +180,6 @@ app.get('/blogPost/:id', function(req, res){
 })
 
 app.post('/blogPost', function(req, res) {
-	console.log(id)
 	blogPost.findById(id, function(err, post) {
 		post.comments.push({ name: req.body.name, body: req.body.body });
 
@@ -102,7 +192,6 @@ app.post('/blogPost', function(req, res) {
 
 app.get('/blogPostList', (req, res)=> {
 	blogPost.find(function(err, data) {
-		console.log(data)
 		res.render('allBlogPosts',{data:data, title:data.title, id: data.id})
 	})
 })
